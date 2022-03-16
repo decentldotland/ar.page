00fcd1500fb2a37b8bf86878f6b733d8847cbb64
Jordan Miller <ramnadroj@gmail.com>
2022-03-16 06:30:39 -0400

    Updated title and desciption

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import Head from 'next/head';
 function MyApp({ Component, pageProps }) {
   return <> <Head>
       <title>ar.page</title>
-      <meta name="description" content="ar.page faq" />
+      <meta name="description" content="ar.page | faq" />
       <link rel="icon" href="/favicon.png" />
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes" />
       {/* <meta name="viewport" content="viewport-fit=cover" /> */}
