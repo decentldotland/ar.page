d153aec8972d16462402415ea054ef8d76c7ea0a
Max Knivets <knivets.m@gmail.com>
2022-08-19 19:38:46 -0400

    fix: searchbar theming and error on load

--- components_new/nav/select-search.tsx
+++ components_new/nav/select-search.tsx
@@ -85,12 +85,12 @@ const CustomSelect = ({ options, multiple, disabled, placeholder }:
             // style={{ paddingLeft: "22px" }} 
             value={val} onChange={handleChange} onFocus={() => setShow(true)} 
             placeholder={placeholder}
-            className="input-bordered pr-4 ml-5 pl-6 w-10/12 h-2 font-sans leading-3 font-light py-4 rounded-none focus:outline-none border-0 mb-0.5 focus:mb-0 focus:border-b-2 focus:border-primary" />
+            className="bg-base-100 input-bordered pr-4 ml-5 pl-6 w-10/12 h-2 font-sans leading-3 font-light py-4 rounded-none focus:outline-none border-0 mb-0.5 focus:mb-0 focus:border-b-2 focus:border-primary" />
             {show && (
                 <div className="h-fit mt-1 p-2 bg-base-100 rounded-md w-[200px]">
                     <ul className="h-full my-2">
                         {snapshot.options
-                            .filter((i: any) => i.name.toLowerCase().includes(val.toLowerCase()))
+                            .filter((i: any) => i.name?.toLowerCase()?.includes(val?.toLowerCase()))
                             .slice(0, 3)
                             .map((option) => (
                                 <li key={option.name} className="w-full mt-1 rounded-md" onClick={
