e75303c9e3604c094dfb9721e0899fb599155370
Max Knivets <knivets.m@gmail.com>
2022-08-22 22:00:38 -0400

    feat: dark mode finalized and profile button now works

--- components_new/layout/index.tsx
+++ components_new/layout/index.tsx
@@ -1,6 +1,8 @@
 // @flow 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Nav } from '../../components_new/nav';
+import Script from 'next/script';
+
 type Props = {
     children: JSX.Element;
 
@@ -8,19 +10,47 @@ type Props = {
 export const Layout = (props: Props) => {
 
     const [isDark, setIsDark] = useState(false);
-
+    const [loaded, setLoaded] = useState(false);
     const toggleDark = () => {
+        if (isDark) {
+            localStorage.theme = 'arlight';
+        } else {
+            localStorage.theme = 'ardark';
+        }
         setIsDark(!isDark);
     }
 
+    useEffect(() => {
+        // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+        if (localStorage.theme === 'ardark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+          setIsDark(true)
+        } else {
+          setIsDark(false)
+        }
+        setLoaded(true)
+    }, [isDark]);
+
     return (
-        <div className="flex flex-col flex-wrap font-mono w-full h-screen" data-theme={isDark ? "ardark" : "arlight"}>
-            <Nav toggleDark={toggleDark} />
-            <div className="h-body grow md:overflow-y-auto overflow-y-scroll">
-            <div id="top"className="h-0 w-0"></div>
-            {props.children}
-            </div>
-        </div>
+        <>
+            {loaded && 
+                <div className="flex flex-col flex-wrap font-mono w-full h-screen" data-theme={isDark ? "ardark" : "arlight"}>
+                    <Nav toggleDark={toggleDark} />
+                    <Script strategy="beforeInteractive">
+                        {`
+                            if (localStorage.theme === 'ardark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                                localStorage.theme = 'ardark'
+                            } else {
+                                localStorage.theme = 'arlight'
+                            }
+                        `}
+                    </Script>
+                    <div className="h-body grow md:overflow-y-auto overflow-y-scroll">
+                    <div id="top"className="h-0 w-0"></div>
+                        {props.children}
+                    </div>
+                </div>
+            }
+        </>
     );
 };
 
--- components_new/user/lpanel/items.tsx
+++ components_new/user/lpanel/items.tsx
@@ -1,22 +1,23 @@
 // @flow 
 import * as React from 'react';
+import { useAns } from 'ans-for-all';
 import { Button } from '../../buttons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faDollarSign, faGift } from '@fortawesome/free-solid-svg-icons';
 import styles from '../../../styles/templates';
-import { faStaylinked } from '@fortawesome/free-brands-svg-icons';
 
-type Props = {
-    
-};
-export const Items = (props: Props) => {
+export const Items = () => {
+    const {
+        walletConnected,
+        ansData,
+    } = useAns();
+
     return (
         <div className="grid h-[65px] mt-4 mx-5">
-            <button className="btn btn-sm btn-primary mb-2">
-                <FontAwesomeIcon icon={faUser} className="w-3 mb-0.5 mr-2"/>
-                <span className="">Profile</span>
-            </button>
-
+            <a href={walletConnected ? ansData?.currentLabel: "/"} className="btn btn-sm btn-primary mb-3">
+                <FontAwesomeIcon icon={faUser} className="w-3 mb-0.5 mr-2" />
+                <span>Profile</span>
+            </a>
             {/* <Button selected className={styles.lpanel.items.buttons.button} text={
                 <div className={styles.lpanel.items.buttons.div}>
                 <div className={styles.lpanel.items.buttons.divDiv}>
