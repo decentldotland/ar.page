7c00eeffc2db219164c4cee5b8e331974dbd27b0
philipdaquin <philipdaquin20@gmail.com>
2022-09-04 13:39:36 +0800

    Replaced old buttons + search  with the newest ui version

---
+++ components_new/nav/SearchBar.tsx
@@ -0,0 +1,18 @@
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+import React from 'react'
+
+function SearchBar() {
+  return (
+    <div className="">
+        <div className='px-4 flex flex-row space-x-3.5 w-[336px] py-2 items-center bg-gray-200 rounded-xl'>
+            <MagnifyingGlassIcon height={20} width={20} strokeWidth={3} color="gray" />
+            <input 
+                type="text" 
+                placeholder='Search for name or address' 
+                className='bg-gray-200 font-inter w-full text-sm font-normal outline-none '/>
+        </div>
+    </div>
+  )
+}
+
+export default SearchBar
