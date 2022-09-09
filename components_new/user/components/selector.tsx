7046d253050c386810092c24e6f90c08982f509c
philipdaquin <philipdaquin20@gmail.com>
2022-09-10 05:51:12 +0800

    adding conditional css backi in, removed text-black

--- components_new/user/components/selector.tsx
+++ components_new/user/components/selector.tsx
@@ -9,13 +9,13 @@ export function Selector(props: {items: item[], selected: number, setSelected: (
   const { items, selected, setSelected } = props;
 
   const [active, setActive] = useState(true)
-//${selected === index ? ('font-semibold'): ('font-medium')}
+
   return (
     <div className="w-full font-inter mb-6">
       <div className="tabs carousel !flex-nowrap"> {/* flex-nowrap is to make tabs work with carousel */}
         {items.map((item, index) => (
           <div key={index} className={`${selected === index && "tab-active"} carousel-item tab p-0 mr-4`} onClick={() => setSelected(index)}>
-            <span key={index} className={`text-xl text-black   `}>{item.name}</span>
+            <span key={index} className={`text-xl ${selected === index ? ('font-semibold'): ('font-medium')}  `}>{item.name}</span>
             <span className=" ml-2 bg-base-200 rounded-lg px-2 py-0.5 text-[#666] text-sm">{item.total}</span>
           </div>
         ))}
