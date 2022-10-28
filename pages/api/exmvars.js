fbbd6d4b3eb41e74d76bdc390f2a36b22b3070b0
Max Knivets <knivets.m@gmail.com>
2022-10-28 03:40:38 -0400

    fix: new contract

--- pages/api/exmvars.js
+++ pages/api/exmvars.js
@@ -1,2 +1,2 @@
-export const contractAddress = "QHJ3EeCJokrf1nbnhMgoK4P_hu9xLPK5UfJww23HFsk" // woah, where did it go?
+export const contractAddress = "kye-I4XnUb5isVO0KX9Edp3Bjq-uv3RuWwnUkF9MRmU" // woah, where did it go?
 export const token = process.env.EXM_TOKEN
