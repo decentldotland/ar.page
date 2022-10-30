9432421b65c91514e908b6e24a7163c01c4c71ba
philipdaquin <philipdaquin20@gmail.com>
2022-10-31 07:52:47 +0800

    updated token

--- pages/api/exmvars.js
+++ pages/api/exmvars.js
@@ -1,2 +1,2 @@
-export const contractAddress = "kye-I4XnUb5isVO0KX9Edp3Bjq-uv3RuWwnUkF9MRmU" // woah, where did it go?
+export const contractAddress = "5Kf8dbUbqMsaqZKGqj9zp0yjaP-pBO57FQbJ0iyGYT0" // woah, where did it go?
 export const token = process.env.EXM_TOKEN
