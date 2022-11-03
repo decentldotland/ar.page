5e39a72d810a8feead11173d17bc2a4abcf24c8b
philipdaquin <philipdaquin20@gmail.com>
2022-11-03 14:04:08 +0800

    avatar selection

---
+++ components_new/onboarding_screens/AvatarSelectionPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+interface Props { 
+    setCurrentStep: any,
+    currentStep: number
+}
+function AvatarSelectionPage({
+    setCurrentStep, 
+    currentStep
+}: Props) {
+  return (
+    <div>AvatarSelectionPage</div>
+  )
+}
+
+export default AvatarSelectionPage
