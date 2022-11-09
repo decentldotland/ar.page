64aab5eaaf236750e2e40b2365f230b352d435e6
philipdaquin <philipdaquin20@gmail.com>
2022-11-09 12:13:14 +0800

    sign uppage

--- components_new/onboarding_screens/SignUpPage.tsx
+++ components_new/onboarding_screens/SignUpPage.tsx
@@ -8,15 +8,23 @@ interface Props {
 
 function SignUpPage({setCurrentStep}: Props) {
   return (
-    <div className=''>
-        <div className="items-center flex flex-col justify-center h-screen space-y-4">
+    <div className='items-center flex flex-row  justify-center'>
+        <div className="items-center flex flex-col justify-center h-screen space-y-3">
           <h1 className='font-bold text-4xl text-center'>
-            Sign Up for ANS 
+            Sign Up for ArPage 
           </h1>
           <h2 className='text-sm text-[#8e8e8f] text-center '>
             Create a profile, follow other accounts, join DAO communities and more. 
           </h2>
-          <CustomConnectButton setCurrentStep={setCurrentStep}/>
+          <div  className='items-center flex flex-col justify-center'>
+          
+          <button onClick={() => setCurrentStep(1)} className=" mt-9 bg-[#1273ea] w-[276px] h-14 items-center rounded-full text-white font-bold text-lg" >
+                <div className='flex justify-center'>
+                  <p className='relative text-center '>Create New Profile</p>
+                </div>
+            </button>
+          </div>
+
           <Link href={"/"} >
             <h1 className='cursor-pointer text-base text-[#8e8e8f] text-center font-medium'>
               Back to Home Page
