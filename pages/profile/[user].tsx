4a1bf6ea880ed22cf0ccb36cba9082d61a03fb14
Max Knivets <knivets.m@gmail.com>
2022-11-18 07:53:07 -0500

    fix: trigger deploy

--- pages/profile/[user].tsx
+++ pages/profile/[user].tsx
@@ -41,7 +41,7 @@ User.getInitialProps = async ({ query }: { query: { user: string; } }) => {
   try {
     if (!query.user) return
     const res = await axios.get(`http://ans-stats.decent.land/profile/${query.user}`);
-    const userInfo = res.data;  // <-- Access one more data object here
+    const userInfo = res.data; // <-- Access one more data object here
     return { pathFullInfo: userInfo };
   } catch (error) {
     console.log("attempting to use domain routing...")
