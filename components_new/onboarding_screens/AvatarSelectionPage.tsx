a8fad0af34c9528af4e8dc21df1d6dec48e1a4b5
philipdaquin <philipdaquin20@gmail.com>
2022-12-01 15:25:51 +0800

    added loading period when image is selected

--- components_new/onboarding_screens/AvatarSelectionPage.tsx
+++ components_new/onboarding_screens/AvatarSelectionPage.tsx
@@ -1,16 +1,15 @@
 import { ArrowLongRightIcon, PlusIcon } from '@heroicons/react/24/outline'
-import React from 'react'
+import React, { useState } from 'react'
 import { useRecoilState, useRecoilValue } from 'recoil';
-import { avatarModalState, userOnboardingState } from '../../atoms';
-import UserBackButton from '../buttons/UserBackButton'
-import BackButton from '../reservation/BackButton'
+import { avatarModalState, selectedAvatar, userOnboardingState } from '../../atoms';
 import {MdOutlineAddAPhoto, MdOutlineAddPhotoAlternate} from 'react-icons/md'
 import MainNextButton from '../buttons/MainNextButton';
 import ModalAvatarSelection from './ModalAvatarSelection';
+import Image from 'next/image';
+import { ARWEAVE_URL } from '../../src/constants';
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
-
-interface Props { 
-}
 
 function AvatarSelectionPage() {
     // Modal State for avatar selection 
@@ -19,6 +18,8 @@ function AvatarSelectionPage() {
     
     // user onboarding step 
     const [userOnboardingStep, setUserOnboarding] = useRecoilState(userOnboardingState);
+    const [currentSelectedAvatar, setSelectedAvatar] = useRecoilState(selectedAvatar);
+    const [isImageReady, setIsImageReady] = useState(false)
 
     
 
@@ -34,9 +35,24 @@ function AvatarSelectionPage() {
                     </p>
                 </div>
                 <div onClick={() => setShowModal(true)} className='flex flex-row justify-center mt-8'>
-                    <div className='cursor-pointer rounded-full w-[237px] h-[237px] bg-[#edecec] items-center flex flex-row justify-center'>
-                        <MdOutlineAddPhotoAlternate size={25} color={"#6a6b6a"} />
-                    </div>
+                    {
+                        currentSelectedAvatar ? (
+                            <>
+                                {!isImageReady && (<Skeleton circle width={237} height={237}/>)}
+                               <Image 
+                                    hidden={isImageReady}
+                                    src={ARWEAVE_URL + currentSelectedAvatar.id} 
+                                    height={237} 
+                                    onLoad={() => setIsImageReady(true)}
+                                    width={237} 
+                                    objectFit='cover' className='rounded-full bg-[#edecec]' />    
+                            </>
+                        ) : (
+                            <div  className='cursor-pointer rounded-full w-[237px] h-[237px] bg-[#edecec] items-center flex flex-row justify-center'>
+                                <MdOutlineAddPhotoAlternate size={25} color={"#6a6b6a"} />
+                            </div>
+                        )
+                    }
                 </div>
                 <h1 className='text-center mt-10 text-sm text-[#8e8e8f] cursor-pointer'>or choose from Decent Land Collection</h1>
             </div>
@@ -55,7 +71,7 @@ function AvatarSelectionPage() {
             </div>
             */}
             <div className='relative bottom-[71px]'>
-                <MainNextButton btnName='Next' disabled={true}/>
+                <MainNextButton btnName='Next' disabled={!currentSelectedAvatar}/>
                 <h1 onClick={() => setUserOnboarding(userOnboardingStep + 1)} className='font-bold text-center mt-6 text-sm text-[#8e8e8f] cursor-pointer'>Skip</h1>
             </div>
         </section>
